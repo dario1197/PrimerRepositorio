702913958b5f69436ea0b428b9e365f4545fec9b
Dario <dario1del1@gmail.com>
2023-11-02 09:29:52 +0100

    cuarto commit

---
+++ Programacion/Ejercicio6.java
@@ -0,0 +1,30 @@
+package EjerciciosBucles;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num;
+        int suma=0;
+        double media;
+        int contador=0;
+        do{
+            System.out.println("Dime un numero");
+            num=sc.nextInt();
+            contador++;
+            suma+=num;
+            
+        }while(num>0);
+        System.out.println("La media es: "+(suma/(contador-1)));
+    }
+    
+}
